27c26afd19ddd292c79103d185ac1a1f9bb0ac82
Nicolas <jumeaucourtn@gmail.com>
2021-05-26 14:39:02 +0200

    Poste de développement unique préparé (iMac)

--- migrations/Version20210526123657.php
+++ migrations/Version20210526123657.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210524084337 extends AbstractMigration
+final class Version20210526123657 extends AbstractMigration
 {
     public function getDescription(): string
     {
