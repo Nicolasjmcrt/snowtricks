173f83e93d9460864f95e9c8c4f9a71bd8e23875
Nicolas <jumeaucourtn@gmail.com>
2021-06-30 16:41:02 +0200

    Add tricks to get 20

--- src/DataFixtures/MediaFixtures.php
+++ src/DataFixtures/MediaFixtures.php
@@ -39,7 +39,7 @@ class MediaFixtures extends Fixture implements DependentFixtureInterface
         $manager->persist($media);
 
         $media = new Media();
-        $media->setCaption('Bloddy Dracula 1');
+        $media->setCaption('Bloddy Dracula Visuel 1');
         $media->setFileName('bloody_dracula_1.jpg');
         $media->setDisplayOrder(1);
 
@@ -50,12 +50,100 @@ class MediaFixtures extends Fixture implements DependentFixtureInterface
         $manager->persist($media);
 
         $media = new Media();
-        $media->setCaption('Bloddy Dracula 2');
+        $media->setCaption('Bloddy Dracula Visuel 2');
         $media->setFileName('bloody_dracula_2.jpg');
         $media->setDisplayOrder(2);
 
         // this reference returns the Group object created in GroupFixtures
         $media->setTrick($this->getReference(TrickFixtures::TRICK_BLOODY_DRACULA));
+
+
+        $manager->persist($media);
+
+        $media = new Media();
+        $media->setCaption('Chicken Salad Visuel 1');
+        $media->setFileName('chicken_salad_1.jpg');
+        $media->setDisplayOrder(1);
+
+        // this reference returns the Group object created in GroupFixtures
+        $media->setTrick($this->getReference(TrickFixtures::TRICK_CHICKEN_SALAD));
+
+
+        $manager->persist($media);
+
+        $media = new Media();
+        $media->setCaption('Chicken Salad Visuel 2');
+        $media->setFileName('chicken_salad_2.jpg');
+        $media->setDisplayOrder(2);
+
+        // this reference returns the Group object created in GroupFixtures
+        $media->setTrick($this->getReference(TrickFixtures::TRICK_CHICKEN_SALAD));
+
+
+        $manager->persist($media);
+
+        $media = new Media();
+        $media->setCaption('Drunk Driver Visuel 1');
+        $media->setFileName('drunk_driver_1.jpg');
+        $media->setDisplayOrder(1);
+
+        // this reference returns the Group object created in GroupFixtures
+        $media->setTrick($this->getReference(TrickFixtures::TRICK_DRUNK_DRIVER));
+
+
+        $manager->persist($media);
+
+        $media = new Media();
+        $media->setCaption('Drunk Driver Visuel 2');
+        $media->setFileName('drunk_driver_2.jpg');
+        $media->setDisplayOrder(2);
+
+        // this reference returns the Group object created in GroupFixtures
+        $media->setTrick($this->getReference(TrickFixtures::TRICK_DRUNK_DRIVER));
+
+
+        $manager->persist($media);
+
+        $media = new Media();
+        $media->setCaption('Roast Beef Visuel 1');
+        $media->setFileName('roast_beef_1.jpg');
+        $media->setDisplayOrder(1);
+
+        // this reference returns the Group object created in GroupFixtures
+        $media->setTrick($this->getReference(TrickFixtures::TRICK_ROAST_BEEF));
+
+
+        $manager->persist($media);
+
+        $media = new Media();
+        $media->setCaption('Roast Beef Visuel 2');
+        $media->setFileName('roast_beef_2.jpg');
+        $media->setDisplayOrder(2);
+
+        // this reference returns the Group object created in GroupFixtures
+        $media->setTrick($this->getReference(TrickFixtures::TRICK_ROAST_BEEF));
+
+
+        $manager->persist($media);
+
+        $media = new Media();
+        $media->setCaption('Tail-grab Visuel 1');
+        $media->setFileName('tail_grab_1.jpg');
+        $media->setDisplayOrder(1);
+
+        // this reference returns the Group object created in GroupFixtures
+        $media->setTrick($this->getReference(TrickFixtures::TRICK_TAIL_GRAB));
+
+
+        $manager->persist($media);
+
+        $media = new Media();
+        $media->setCaption('Tail-grab Visuel 2');
+        $media->setFileName('tail_grab_2.jpg');
+        $media->setDisplayOrder(2);
+
+        // this reference returns the Group object created in GroupFixtures
+        $media->setTrick($this->getReference(TrickFixtures::TRICK_TAIL_GRAB));
 
 
         $manager->persist($media);
@@ -149,6 +237,50 @@ class MediaFixtures extends Fixture implements DependentFixtureInterface
         $manager->persist($media);
 
         $media = new Media();
+        $media->setCaption('Frontside Rodeo Visuel 1');
+        $media->setFileName('frontside_rodeo_1.jpg');
+        $media->setDisplayOrder(1);
+
+        // this reference returns the Group object created in GroupFixtures
+        $media->setTrick($this->getReference(TrickFixtures::TRICK_FRONTSIDE_RODEO));
+
+
+        $manager->persist($media);
+
+        $media = new Media();
+        $media->setCaption('Frontside Rodeo Visuel 2');
+        $media->setFileName('frontside_rodeo_2.jpg');
+        $media->setDisplayOrder(2);
+
+        // this reference returns the Group object created in GroupFixtures
+        $media->setTrick($this->getReference(TrickFixtures::TRICK_FRONTSIDE_RODEO));
+
+
+        $manager->persist($media);
+
+        $media = new Media();
+        $media->setCaption('Mc Twist Visuel 1');
+        $media->setFileName('mc_twist_1.jpg');
+        $media->setDisplayOrder(1);
+
+        // this reference returns the Group object created in GroupFixtures
+        $media->setTrick($this->getReference(TrickFixtures::TRICK_MC_TWIST));
+
+
+        $manager->persist($media);
+
+        $media = new Media();
+        $media->setCaption('Mc Twist Visuel 2');
+        $media->setFileName('mc_twist_2.jpg');
+        $media->setDisplayOrder(2);
+
+        // this reference returns the Group object created in GroupFixtures
+        $media->setTrick($this->getReference(TrickFixtures::TRICK_MC_TWIST));
+
+
+        $manager->persist($media);
+
+        $media = new Media();
         $media->setCaption('Quad Cork 1800 Visuel 1');
         $media->setFileName('quad_cork_1800_1.jpg');
         $media->setDisplayOrder(1);
@@ -210,6 +342,50 @@ class MediaFixtures extends Fixture implements DependentFixtureInterface
 
         // this reference returns the Group object created in GroupFixtures
         $media->setTrick($this->getReference(TrickFixtures::TRICK_NOSEPRESS));
+
+
+        $manager->persist($media);
+
+        $media = new Media();
+        $media->setCaption('Gutterball Visuel 1');
+        $media->setFileName('gutterball_1.jpg');
+        $media->setDisplayOrder(1);
+
+        // this reference returns the Group object created in GroupFixtures
+        $media->setTrick($this->getReference(TrickFixtures::TRICK_GUTTERBALL));
+
+
+        $manager->persist($media);
+
+        $media = new Media();
+        $media->setCaption('Gutterball Visuel 2');
+        $media->setFileName('gutterball_2.jpg');
+        $media->setDisplayOrder(2);
+
+        // this reference returns the Group object created in GroupFixtures
+        $media->setTrick($this->getReference(TrickFixtures::TRICK_GUTTERBALL));
+
+
+        $manager->persist($media);
+
+        $media = new Media();
+        $media->setCaption('Lipslide Visuel 1');
+        $media->setFileName('lipslide_1.jpg');
+        $media->setDisplayOrder(1);
+
+        // this reference returns the Group object created in GroupFixtures
+        $media->setTrick($this->getReference(TrickFixtures::TRICK_LIPSLIDE));
+
+
+        $manager->persist($media);
+
+        $media = new Media();
+        $media->setCaption('Lipslide Visuel 2');
+        $media->setFileName('lipslide_2.jpg');
+        $media->setDisplayOrder(2);
+
+        // this reference returns the Group object created in GroupFixtures
+        $media->setTrick($this->getReference(TrickFixtures::TRICK_LIPSLIDE));
 
 
         $manager->persist($media);
